7a3940a3136e0fc7742ff0ba79d4bd787689db3f
Yaswantsoni1128 <yaswantsoni2005@gmail.com>
2025-04-28 13:25:35 +0530

    stage 1

--- Yardstick/src/utils/validators.js
+++
@@ -1,9 +0,0 @@
-export function validateTransaction(transaction) {
-  if (!transaction.amount || !transaction.description || !transaction.date) {
-    return "All fields are required.";
-  }
-  if (transaction.amount <= 0) {
-    return "Amount must be greater than zero.";
-  }
-  return null;
-}
